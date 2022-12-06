89495bc35d2ac7defd25facb7ab4d0b06f3bb653
ibkam <ibrahimkamau254@gmail.com>
2022-12-06 13:54:59 -0800

    FizzBuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    element = ""
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            element = "FizzBuzz"
+        elif num % 3 == 0:
+            element = "Fizz"
+        elif num % 5 == 0:
+            element = "Buzz"
+        else:
+            element = f"{num}"
+
+        if num < 100:
+                print("{} ".format(element), end="")
+        else:
+                print("{} ".format(element), end="")
