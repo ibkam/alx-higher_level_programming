814e24468536e5ad7a34912a18e6698354341b66
ibkam <ibrahimkamau254@gmail.com>
2022-12-06 04:59:46 -0800

    Two separate digit

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for number in range(0, 10):
+    for i in range(number + 1, 10):
+        if number == 8 and i== 9:
+            print("{}{}".format(number, i))
+        else:
+            print("{}{}, ".format(number, i), end="")
