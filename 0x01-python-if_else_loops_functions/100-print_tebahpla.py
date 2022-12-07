947e30410ccc657471387fef0097f257bf641dce
ibkam <ibrahimkamau254@gmail.com>
2022-12-07 03:13:26 -0800

    Alterinatingcase Alphabet in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -5,5 +5,5 @@ for i in reversed(range(97, 123)):
     if i % 2 == 0:
         alphabet = chr(i)
     else:
-        alphabet = chr(i -32)
+        alphabet = chr(i - 32)
     print("{}".format(alphabet), end="")
