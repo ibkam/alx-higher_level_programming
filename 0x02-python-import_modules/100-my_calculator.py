9a3ecc3c28adad4fca7a04857de90443f10a57d5
ibkam <ibrahimkamau254@gmail.com>
2022-12-10 09:21:03 -0800

    Imports function from cal and handles basic operations

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+
+    if len(argv) - 1 != 3:
+        print("usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif argv[2] not in ops:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        # converting input parameters to intergers
+        a = int(argv[1])
+        b = int(argv[3])
+
+        # handling operators
+        if argv[2] == "+":
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        elif argv[2] == "-":
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        elif argv[2] == "*":
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        elif argv[2] == "/":
+            print("{} / {} = {}".format(a, b, div(a, b)))
+
