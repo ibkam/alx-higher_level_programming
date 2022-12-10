2d2bbe9db62cf3eb71921568992e11e9c706dd05
ibkam <ibrahimkamau254@gmail.com>
2022-12-10 05:52:06 -0800

    Sum argument list

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     elif (len(argv) == 2):
         print(argv[1])
     else:
-        # duplicate the loop in iterate through argument list to convert int
-        dup = [int(argv[i]) for i in range(1, len(argv))]
-        # sum is the iteration of list of argument
-        Sum = sum(dup)
+        # iterate through argument list
+        argv_count = [int(argv[i]) for i in range(1, len(argv))]
+        # sum the iterate list
+        Sum = sum(argv_count)
         print(Sum)
