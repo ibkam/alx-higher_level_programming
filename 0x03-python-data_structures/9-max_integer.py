0c459abd39e3c91012b83984bf792d650fce4b5a
ibkam <ibrahimkamau254@gmail.com>
2022-12-15 06:52:12 -0800

    Returns biggest integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+    max_integer = my_list[0]
+
+    for integer in my_list:
+        if integer > max_integer:
+            max_integer = integer
+    return (max_integer)
