eb8086bf55dca399d67ad8a555641413dda2268e
ibkam <ibrahimkamau254@gmail.com>
2022-12-28 02:10:16 -0800

    Print x element with indexerror

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/pyton3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in range(x): #x rep the number of element to print
+        try:
+            print("{}".format(my_list[i]), end="")
+        except IndexError: # x can be bigger than the length of my_list
+            break
+        else:
+            count += 1
+    print()
+    return count
