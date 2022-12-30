08660e7e4d10a2f49be79a0bba60e88b599980f5
ibkam <ibrahimkamau254@gmail.com>
2022-12-30 04:18:17 -0800

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""creating a square class"""
+
+
+class Square:
+    """intializing the class attribute"""
+    def __init__(self, size=0):
+        self.__size = size
+    """bundling variable size with function and
+        retrive variable size"""
+    @property
+    def size(self):
+        return self.__size
+
+    """And set it according to the value"""
+    @size.setter
+    def size(self, value):
+        if(type(value) is not int):
+            raise TypeError("size must be an interger")
+        elif(value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    
+    """area of the square"""
+    def area(self):
+            return self.__size ** 2
