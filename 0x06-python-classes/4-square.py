989a2e957adfe20092daef8f6ac39e321955c2ae
ibkam <ibrahimkamau254@gmail.com>
2022-12-30 06:35:12 -0800

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -20,7 +20,7 @@ class Square:
         elif(value < 0):
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     """area of the square"""
     def area(self):
-            return self.__size ** 2
+        return self.__size ** 2
