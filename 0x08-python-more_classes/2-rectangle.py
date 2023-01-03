7b18b0292a89f2023f5dfa780660507a614c71fa
ibkam <ibrahimkamau254@gmail.com>
2023-01-03 14:11:58 -0800

    Area and Perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,15 +3,15 @@
 
 
 class Rectangle:
-    """this represent a rectangle"""
+    """this represents a rectangle"""
 
     def __init__(self, width=0, height=0):
-        """Intializing rectangle class
+        """Initializing this rectangle class
         Args:
             width: represents the width of the rectangle
-            height: represent the height of the rectangle
+            height: represents the height of the rectangle
         Raises:
-            TypeError: if size is not an interger
+            TypeError: if size is not integer
             ValueError: if size is less than zero
         """
         self.width = width
@@ -19,28 +19,28 @@ class Rectangle:
 
     @property
     def width(self):
-        """retrieves width attributes"""
+        """retrieves width attribute"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """sets width attributes"""
+        """sets width attribute"""
         if not isinstance(value, int):
-            raise TypeError("width must be an interger")
+            raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """retrieves heigh attributes"""
-        return self.height
+        """retrieves height attribute"""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """sets height attributes"""
+        """sets height attribute"""
         if not isinstance(value, int):
-            raise TypeError("height must be an interger")
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
