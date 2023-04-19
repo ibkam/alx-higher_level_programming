98042281df3d107bc7ed53ab2235e051743dea96
ibkam <110990848+ibkam@users.noreply.github.com>
2023-04-19 12:17:19 +0300

    if the object is an instance of a class that inherited

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+function that returns true if the object is an instance of the class
+that inherited form the specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    returns true if object is an instance of a class
+    else false
+    """
+    if issubclass(obj, a_class)
+        return True
+    return False
