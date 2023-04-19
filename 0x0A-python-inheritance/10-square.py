931e3faa6fa5d1a5340e5884f8c2fa73423c408d
ibkam <110990848+ibkam@users.noreply.github.com>
2023-04-19 15:34:25 +0300

    Square #1
    a class Square that inherits from Rectangle (9-rectangle.py):
    
        Instantiation with size: def __init__(self, size)::
            size must be private. No getter or setter
            size must be a positive integer, validated by integer_validator
        the area() method must be implemented

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" class square inherits from Rectangle(9-rectangle.py)"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+class Square(Rectangle):
+    """ class square"""
+    def __init__(self, size):
+      """intialize a new square.
+      Args:
+            size (int): The size of the new square.
+      """
+      self.integer_validator("size", size)
+      super().__init__(size, size)
+      self.__size = size
