1d3678d8065fb4386e30d3f55fa7633c65fb4d17
ibkam <110990848+ibkam@users.noreply.github.com>
2023-04-24 14:25:47 +0300

    update

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""modue:class student"""
+
+"""Defines a class Student."""
 
 
-class Student():
-    """Rep a student"""
+class Student:
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """Intialize a new Student
+        """Initialize a new Student.
         Args:
-            @first_name: the first name
-            @second_name: the second name
-            @age: the age
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-     def to_json(self):
+
+    def to_json(self):
+        """Get a dictionary representation of the Student."""
         return self.__dict__
