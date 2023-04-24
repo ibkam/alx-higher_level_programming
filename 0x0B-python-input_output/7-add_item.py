cedaa04e3d65816def29248f87bbf667a424a03b
ibkam <110990848+ibkam@users.noreply.github.com>
2023-04-24 11:47:37 +0300

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""module: function"""
-
+"""Add all arguments to a Python list and save them to a file"""
 import sys
+
 if__name__ == "__main__":
-    save_json = __import__("5-save_to_json").save_to_json_file
-    load_json = __import__("6-load_from_json").load_to_json_file
+    save_json = __import__("5-save_to_json__file").save_to_json_file
+    load_json = __import__("6-load_from_json__file").load_from_json_file
 
     try:
         items = load_json("add_item.json")
