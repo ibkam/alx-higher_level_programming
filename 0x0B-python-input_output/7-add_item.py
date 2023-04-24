50e41fc48efd79d21af651ad8487c29c2f0f7771
ibkam <110990848+ibkam@users.noreply.github.com>
2023-04-24 11:50:49 +0300

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,7 @@
 """Add all arguments to a Python list and save them to a file"""
 import sys
 
-if__name__ == "__main__":
+if __name__ == "__main__":
     save_json = __import__("5-save_to_json__file").save_to_json_file
     load_json = __import__("6-load_from_json__file").load_from_json_file
 
