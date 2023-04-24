42ab8e4f69f3874b735832028bbeb6e4cb958020
ibkam <110990848+ibkam@users.noreply.github.com>
2023-04-24 12:02:32 +0300

    Class to JSON
    A function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object:

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module: returns the dict description with data structure"""
+
+def class_to_json(obj):
+  """returns dictionary description
+  Args:
+      @obj: object passed
+      
+  Returns: the dict in data structure
+  """
+  return obj.__dict__
