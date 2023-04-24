5eee403981449b1bcf2dd07d8da509fbd0c9c7b5
ibkam <110990848+ibkam@users.noreply.github.com>
2023-04-24 14:18:58 +0300

    Student to JSON
    a class Student that defines a student by:
    
        Public instance attributes:
            first_name
            last_name
            age
        Instantiation with first_name, last_name and age: def __init__(self, first_name, last_name, age):
        Public method def to_json(self): that retrieves a dictionary representation of a Student instance (same as 8-class_to_json.py)
        You are not allowed to import any module

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""modue:class student"""
+
+
+class Student():
+    """Rep a student"""
+    
+    def __init__(self, first_name, last_name, age):
+        """Intialize a new Student
+        
+        Args:
+            @first_name: the first name
+            @second_name: the second name
+            @age: the age
+            
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        
+    def t-_json(self):
+        return self.__dict__
