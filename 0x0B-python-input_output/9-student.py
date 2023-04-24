95c519cc0e29829b9bd5f71430eafeb9fba2f48c
ibkam <110990848+ibkam@users.noreply.github.com>
2023-04-24 14:24:44 +0300

    update

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -10,11 +10,9 @@ class Student():
             @first_name: the first name
             @second_name: the second name
             @age: the age
-            
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
-    def to_json(self):
+     def to_json(self):
         return self.__dict__
