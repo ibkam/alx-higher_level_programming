602c47bb1b9f268c925c98edcc4a445ebf19b180
ibkam <110990848+ibkam@users.noreply.github.com>
2023-04-24 15:46:03 +0300

    update

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
-"""module: class student"""
 
-class Student():
-	"""Rep a new class student"""
-	def __init__(self, first_name, last_name, age):
-		"""initialize a new student
-		Args:
-			@first_name: the first name of the student
-			@last_name: the last name of the student
-			@age: the age of the student
-		"""
-		self.first_name = first_name
-		self.last_name = last_name
-		self.age = age
-		
-	def to_json(self, attrs=None):
-		"""get dictionary rep of the student
-		If attrs is a list of strings,
-		only attribute names contained in this list must be retrieved.
-		Otherwise, all attributes must be retrieved.
-		
-		Args:
-			@attrs(list): the attributes rep
-			
-		"""
-		if (type(attrs) == list and
-				all(type(ele) == str for ele in attrs)):
-			return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-		return self.__dict__
+"""Defines a class Student."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
