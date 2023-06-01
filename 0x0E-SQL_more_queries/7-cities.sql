21deffd73ff4bb3e7cdd64e17dbfaa0072cfad37
ibkam <110990848+ibkam@users.noreply.github.com>
2023-06-01 17:58:30 +0300

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,8 @@
 -- creates the database hbtn_0d_usa and the table states
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-	PRIMARY (`id`),
-	`id` INT UNIQUE NOT NULL AUTO_INCREMENT,
-	`state_id` INT NOT NULL,
-	`name` VARCHAR(256) NOT NULL
-	FOREIGN KEY (`state_id`) REFERENCE `hbtn_0d_usa`.`state`(`id`)
-	);
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+  `id` INT UNIQUE NOT NULL AUTO_INCREMENT,
+  `state_id` INT NOT NULL,
+  `name` VARCHAR(256) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`));
