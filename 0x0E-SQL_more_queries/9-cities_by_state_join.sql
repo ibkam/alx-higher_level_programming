ce47ecdcb7e43417e73829b4e818813907cb2506
ibkam <110990848+ibkam@users.noreply.github.com>
2023-06-08 10:35:55 +0300

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,6 +2,5 @@
 -- using inner join
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states 
-ON cities.states.id = states.id
-ORDER BY cities.id;
+INNER JOIN states
+ON cities.state_id = states.id;
