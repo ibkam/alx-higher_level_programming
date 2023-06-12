cae3d270869c54c4c6b9855633eb01e10959b7b5
ibkam <110990848+ibkam@users.noreply.github.com>
2023-06-12 14:21:42 +0300

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,6 @@
-SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, genre_id ASC;
+--  a script that lists all shows contained in the database hbtn_0d_tvshows.
+SELECT tv_shows.title, tv_show_genres.genre_id
+  FROM tv_show_genres
+  RIGHT JOIN tv_shows
+  ON tv_shows.id = tv_show_genres.show_id
+  ORDER BY tv_shows.title, tv_show_genres.genre_id;
