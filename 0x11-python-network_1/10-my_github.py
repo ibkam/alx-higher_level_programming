37b9645117c82c10ba913ef83acfdee77ec940f3
ibkam <110990848+ibkam@users.noreply.github.com>
2023-06-19 14:46:13 +0300

    Create 10-my_github.py
    Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id
    
    You must use Basic Authentication with a personal access token as password to access to your information (only read:user permission is needed)
    The first argument will be your username
    The second argument will be your password (in your case, a personal access token as password)
    You must use the package requests and sys
    You are not allowed to import packages other than requests and sys
    You don’t need to check arguments passed to the script (number or type)

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""takes your GitHub credentials (username and password) and uses the GitHub API to display your id"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+if__name__== "__main__":
+  auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+  request = requests.get("https://api.github.com/user", auth=auth)
+  print(request.json().get("id")
