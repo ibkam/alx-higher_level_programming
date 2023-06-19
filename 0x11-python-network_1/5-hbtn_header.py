23bab95ea0db36038a307ac5a952a23bc75e1dd9
ibkam <110990848+ibkam@users.noreply.github.com>
2023-06-19 10:06:56 +0300

    Create 5-hbtn_header.py
    Python script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header
    
    You must use the packages requests and sys
    You are not allow to import other packages than requests and sys
    The value of this variable is different for each request
    You don’t need to check script arguments (number and type)

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and 
+displays the value of the variable X-Request-Id in the response header
+"""
+import requests
+from sys import agrv
+
+if __name__ == "__main__":
+    url = sys.agrv[1]
+    
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
+    
