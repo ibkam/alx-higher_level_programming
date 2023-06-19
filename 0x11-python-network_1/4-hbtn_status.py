41ddab16701ac5f25cb4fb33e053715472d3d5e0
ibkam <110990848+ibkam@users.noreply.github.com>
2023-06-19 09:53:45 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """script that fetches https://alx-intranet.hbtn.io/status"""
 
-import urllib.request
+import requests
 
 if __name__ == "__main__":
-    request = urlib.request.get('https://alx-intranet.hbtn.io/status')
-    print("Body response:$")
+    request = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
     print("\t- type: {}".format(type(read.text)))
     print("\t- content: {}".format(type(read.text)))
