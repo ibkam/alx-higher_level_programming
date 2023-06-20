10cd26ed55d451c08f4e83aab9f520b082bf98db
ibkam <110990848+ibkam@users.noreply.github.com>
2023-06-20 15:32:59 +0300

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 //  prints a message depending of the number of arguments passed:
 const process = require('process');
-const argv  = process.argv
+const argv  = process.argv;
 if (agrv.length === 2) {
   console.log('No argument');
 }
