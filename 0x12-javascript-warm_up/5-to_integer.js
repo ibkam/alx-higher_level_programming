e5b109d0de8a7e0ea3f4197b71b53127c6c3a3dc
ibkam <110990848+ibkam@users.noreply.github.com>
2023-06-21 22:36:42 +0300

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,6 +2,7 @@
 // prints My number: <first argument converted in integer>const { argv } = require('process');
 const { argv } = require('process');
 const convert = Number(argv[2]);
+
 if (isNaN(convert)) {
 	console.log('Not a number');
 } else {
