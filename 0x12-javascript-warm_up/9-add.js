fb2aeac7c76a719d28401fdf04a57e4f4b4db317
ibkam <110990848+ibkam@users.noreply.github.com>
2023-06-21 11:52:15 +0300

    Create 9-add.js
    script that prints the addition of 2 integers
    
    The first argument is the first integer
    The second argument is the second integer
    You have to define a function with this prototype: function add(a, b)
    You must use console.log(...) to print all output
    You are not allowed to use var

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// prints the addition of 2 integers
+const { argv } = require('process');
+const a = Number(argv[2]);
+const b = Number(argv[3]);
+const sum = 0;
+
+function add (a, b) {
+  console.log(sum = a + b);
+}
+add (a, b);
