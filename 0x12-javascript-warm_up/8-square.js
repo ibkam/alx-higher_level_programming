0126c36c12cb27963130560a30d48816ac0c7646
ibkam <110990848+ibkam@users.noreply.github.com>
2023-06-21 11:31:34 +0300

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-//script that prints a square
+// script that prints a square
 const { argv } = require('process');
 const size = Number(argv[2]);
 const square = 'X'.repeat(size);
 if (isNaN(size)) {
-  console.log('Missing size')
+  console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
     console.log(square);
