29b8acbe0c825426297cfce60057a650215780f3
ibkam <110990848+ibkam@users.noreply.github.com>
2023-06-21 13:04:02 +0300

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,6 +3,6 @@
 const { argv } = require('process');
 const n = argv[2];
 function factorial (n) {
-  return n === 0 || isNaN(n) ? 1 : n * factorial(n-1);
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 console.log(factorial(Number(n)));
