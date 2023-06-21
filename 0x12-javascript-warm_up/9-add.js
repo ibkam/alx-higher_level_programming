1caea943b5c097e167ba112cffce09ed8456deff
ibkam <110990848+ibkam@users.noreply.github.com>
2023-06-21 12:13:50 +0300

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,5 +4,7 @@ const { argv } = require('process');
 const a = Number(argv[2]);
 const b = Number(argv[3]);
 
-function add(a, b) => a + b;
+function add(a, b) {
+  (a + b)
+}
 console.log(add(a,b));
