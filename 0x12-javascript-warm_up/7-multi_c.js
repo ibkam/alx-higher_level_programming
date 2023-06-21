c10e548a1b38e5173133d26fa6d4c66d9f90f6cc
ibkam <110990848+ibkam@users.noreply.github.com>
2023-06-21 11:03:11 +0300

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,11 @@
 //  script that prints x times “C is fun”
 const { argv } = require('process');
 const occurence = Number(argv[2]);
-for ( let i = 0; i < occurence; i++) {
-  console.log('C is fun');
-}
+
 if isNaN(occurence) {
   console.log('Missing number of occurrences');
+} else {
+  for ( let i = 0; i < occurence; i++) {
+    console.log('C is fun');
+  }
 }
