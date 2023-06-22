42471362b36841d5bb19366862d3e3567ff44855
ibkam <110990848+ibkam@users.noreply.github.com>
2023-06-22 13:15:28 +0300

    Update 1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // class Rectangle that defines a rectangle:
 module.exports = class Rectangle {
-  constructor(width, height) {
+  constructor(w, h) {
     this.width = w;
     this.height = h;
   }
