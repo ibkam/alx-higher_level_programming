229586bcdd69189a75339c50e40fd9490ba3eceb
ibkam <110990848+ibkam@users.noreply.github.com>
2023-06-22 12:56:49 +0300

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const { argv } = require('process');
 const convert = Number(argv[2]);
 
-if(isNaN(convert)) {
+if (isNaN(convert)) {
   console.log('Not a number');
 } else {
   console.log(`My number: ${convert}`);
