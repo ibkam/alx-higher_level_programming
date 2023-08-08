aca3745c9fad5641b4316708e41a3d8e14914ea4
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-08 12:48:15 +0300

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,7 +8,7 @@ module.exports = class Rectangle {
   print () {
     for (let i = 0; i < this.width; i++) {
       for (let i =0; i < this.height; i++)
-      console.log('X');
-    }
+        }
+     console.log('X');
   }
 };
