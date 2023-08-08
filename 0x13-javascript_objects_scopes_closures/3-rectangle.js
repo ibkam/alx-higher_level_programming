c27fb044fea6b08a9cbf958d1a283e3d1750cdda
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-08 17:56:01 +0300

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,7 +2,7 @@
 // a class rectangle that defines a rectangle
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h];}
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
   print () {
