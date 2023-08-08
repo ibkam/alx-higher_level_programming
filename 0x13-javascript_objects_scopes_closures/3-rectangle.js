d29b918f9d989519bedbc3a6d209ace051930c54
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-08 12:28:23 +0300

    Create 3-rectangle.js
    A class Rectangle that defines a rectangle:
    You must use the class notation for defining your class
    The constructor must take 2 arguments: w and h
    Initialize the instance attribute width with the value of w
    Initialize the instance attribute height with the value of h
    If w or h is equal to 0 or not a positive integer, create an empty object
    Create an instance method called print() that prints the rectangle using the character X

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// a class rectangle that defines a rectangle
+module.export = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]}
+  }
+
+  print () {
+    for (let i = 0; i < this.width; i++) {
+      console.log('X'.repeat(this.height));
+    }
+  }
+}
