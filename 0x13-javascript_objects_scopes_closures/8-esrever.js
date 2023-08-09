09f6d718b9a9751ec92318e19fb38a5dd87ca9b9
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-09 12:01:36 +0300

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 // Returns the reversed version of a list
 exports.esrever = function (list) {
   const reversedlist = [];
-  for (let i = 0; i < list.length - 1; i >= 0; i--) {
+  for (let i = 0; i < list.length - 1; i--) {
     reversedlist.push(list[i]);
   }
   return reversedlist;
