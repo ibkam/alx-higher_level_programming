9a027a4f452fc219a5f93e872440f52ca9d8fa9a
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-09 15:11:36 +0300

    Update 9-logme.js

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -2,6 +2,6 @@
 // function that prints the number of arguments already printed and the new argument value
 let count = 0;
 exports.logMe = function (item) {
-  console.log(${count} : ${item})
+  console.log(`${count} : ${item}`)
   count++;
 };
