a4cc573056e3f96319cc289194c19e9f4f292c98
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-09 10:37:45 +0300

    Update 5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // class Square that defines a square
-module.exports = class square extends('/.4-rectangle.js') {
+modules.exports = class square extends('/.4-rectangle.js') {
   constructor (size) {
     super(size, size)
   }
