d8d79859e94321490127faf59919b5579f052532
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-09 18:07:08 +0300

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
+const dict = require('./101-data');
 const newDict = {};
-for (const key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
+
+for (const value in dict) {
+  const occurrences = data[value];
+  if (!newDict[occurrences]) {
+    newDict[occurrences] = [];
   }
+  newDict[occurrences].push(userId);
 }
-console.log(newdict);
+console.log( newDict[occurrences]);
