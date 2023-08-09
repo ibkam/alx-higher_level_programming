2f3e6fa2cc6a5a942d44578d3fb857d24f2b4995
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-09 17:44:38 +0300

    Create 101-sorted.js
    script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
    
    Your script must import dict from the file 101-data.js
    In the new dictionary:
    A key is a number of occurrences
    A value is the list of user ids
    Print the new dictionary at the end

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+//  script that imports a dictionary of occurrences by user id
+const dict = requires('./101-data.js');
+const newdict = {};
+
+for (const value in dict) {
+  const occurrence = dict[key];
+  if (!newdict[occurrence]) {
+    newdict[occurrence] = [];
+  }
+  newdict[occurrence].push(value);
+}
+console.log(newdict);
