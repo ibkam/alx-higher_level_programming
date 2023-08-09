01d35699c5400e47f386c904d73bb1cb6eaa3750
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-09 17:06:18 +0300

    Update 10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,6 +2,6 @@
 // function that converts a number from base 10 to another base passed as argument
 exports.converter = function (base) {
   return function (dec) {
-    return dec.toStrng(base);
+    return dec.toString(base);
   };
 };
