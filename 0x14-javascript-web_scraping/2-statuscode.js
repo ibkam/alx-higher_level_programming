f209a9d7f0136cbdbdd85d6a975aa1d64b0f2543
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-10 20:00:16 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,6 @@
 #!/usr/bin/npde
 // script that display the status code of a GET request.
 const request = require('request');
-const url = process.argv[2];
-
-request.get(url, function (response) {
+request.get(process.argv[2].on('response', function (response) {
   console.log(`code: ${response.statusCode}`)
 });
