0222eb98875c33e7cad76094d9db66719c96a5a2
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-10 18:57:28 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 // script that reads and prints the content of a file.
 const fs = require('fs')
-async function readfilecontent {
+async function readfilecontent(filepath) {
   try {
     const content = await fs.promise.readfile(filepath, 'utf-8');
     console.log(content)
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 }
 if (process.argv.length < 3) {
-  console.log(error);
+  console.error(error);
 } else {
   const filepath = process.argv[2];
   readfilecontent(filepath);
-}
+};
