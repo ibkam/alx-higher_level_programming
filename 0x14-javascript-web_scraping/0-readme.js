0b85555e9e8ea5a45ab57503dccf4f54fbeb0e3e
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-10 19:42:32 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,17 +1,6 @@
 #!/usr/bin/node
-// script that reads and prints the content of a file.
+// a script that reads and prints the content of a file.
 const fs = require('fs');
-async function readFileContent (filepath) {
-  try {
-    const content = await fs.promise.readFile(filepath, 'utf-8');
-    console.log(content);
-  } catch (error) {
-    console.error('ENOENT: no such file or directory, open "doesntexist"', error);
-  }
-}
-if (process.argv.length < 3) {
-  console.error('ENOENT: no such file or directory, open "doesntexist"', error);
-} else {
-  const filepath = process.argv[2];
-  readfilecontent(filepath);
-}
+fs.readFile(process.argv[2], 'utf-8', function (error, content) {
+  console.log(error || content);
+});
