c1331f650e6920b6457d0c4c78194e4569815a17
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-12 17:03:37 +0300

    Create 5-request_store.js
    Write a script that gets the contents of a webpage and stores it in a file.
    The first argument is the URL to request
    The second argument the file path to store the body response
    The file must be UTF-8 encoded
    You must use the module request

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+/usr/bin/node
+// gets the contents of a webpage and stores it in a file
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  fs.writeFile(filePath, body, { encoding: 'utf-8'}, error => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(${filePath})
+    }
+  });
+});
