3e1cb9151eb375f555cb11e7cc31f572d1e77171
ibkam <ibrahimkamau254@gmail.com>
2023-08-12 13:26:01 -0700

    execute

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-// script that imports an array and computes a new array.
-const list = require('./100-data');
-const newlist = list.map((value, index) => value * index);
-
+const list = require('./100-data.js').list;
 console.log(list);
-console.log(newlist);
+console.log(list.map((x, xI) => x * xI));
