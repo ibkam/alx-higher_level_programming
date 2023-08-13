a6beff472242e66f463bf2e75480d31138ed9f95
ibkam <ibrahimkamau254@gmail.com>
2023-08-13 11:11:28 +0000

    Dispaly StatusCode

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 // script that display the status code of a GET request.
 const request = require('request');
-const url = process.argv[2];
-request.get(url, function (response) {
+request.get(process.argv[2]).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
 });
