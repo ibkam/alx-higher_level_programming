58f3ce524eea597ef1186408948b82ce24a56bd0
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-14 12:36:35 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/18'
+const url = 'https://swapi-api.alx-tools.com/api/films/18';
 request(url + process.argv[2], function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
