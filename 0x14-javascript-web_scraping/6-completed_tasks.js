d5837cb921fa4d80d1437754a717cde36b795060
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-14 12:52:32 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,20 +1,16 @@
 #!/usr/bin/node
-// script that computes the number of tasks completed by user id
 const request = require('request');
-const url = process.argv[2];
-
-request(url, (error, body, response) => {
-  if (error) {
-    console.error(error);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
+      }
+    });
+    console.log(completed);
   }
-  const todos = JSON.parse(body);
-  const completed = {};
-  todos.forEach((todo) => {
-    if (todo.completed && completed[todo.userId] === undefined) {
-      completed[todo.userId] = 1;
-    } else if (todo.completed) {
-      completed[todo.userId] += 1;
-    }
-  });
-  console.log(completed);
 });
