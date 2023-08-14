6355bf2258168a5c8592feca9439b227b1d79d87
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-14 12:50:56 +0300

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// gets the contents of a webpage and stores it in a file
-const request = require('request');
 const fs = require('fs');
-request(process.argv[2].pipe(fs.createWriteStream(process.argv[3])));
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
