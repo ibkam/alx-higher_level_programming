d830e3e64b27a8f51597db6082280c469a255ff5
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-16 10:12:36 +0300

    Update 0-script.js
    Update Selector with uppercase

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,2 +1,2 @@
 // Script that updates the text color of the <header> element to red (#FF0000):
-document.querySelector('header').Style.Color = '#FF0000';
+document.querySelector('HEADER').style.Color = '#FF0000';
