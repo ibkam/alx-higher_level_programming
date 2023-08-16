e3501ff850ca5aeec8f10f01adc40b85e583e269
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-16 09:42:09 +0300

    Update 0-script.js

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,2 +1,2 @@
-// cript that updates the text color of the <header> element to red (#FF0000):
-document.querySelector('header').css('color', 'red');
+// Script that updates the text color of the <header> element to red (#FF0000):
+document.querySelector('header').Style.Color = '#FF0000';
