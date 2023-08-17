1ff14fc5320c426908a4d85d09d590207086cf21
ibkam <110990848+ibkam@users.noreply.github.com>
2023-08-17 16:52:46 +0300

    No jQuery - document loaded
    JavaScript script that updates the text color of the <header> element to red (#FF0000):
    
    You must use document.querySelector to select the HTML tag
    You can’t use the jQuery API
    Note: Your script must be imported from the <head> tag, not at the end of the HTML
    Please test with this HTML file in your browser:
    
    guillaume@ubuntu:~/0x15$ cat 100-main.html 
    <!DOCTYPE html>
    <html lang="en">
      <head>
        <title>Holberton School</title>
        <script type="text/javascript" src="100-script.js"></script>
      </head>
      <body>
        <header> 
          First HTML page
        </header>
        <footer>
          Holberton School - 2017
        </footer>
      </body>
    </html>

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,4 @@
+// updates the text color of the <header> element to red (#FF0000)
+document.addEventListener('DOMContentLoaded' ()=> {
+  document.querySelector('HEADER').style.color = '#FF0000';
+})
