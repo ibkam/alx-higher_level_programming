7a73818456431bdd54b5c6b45506e5147e19b955
ibkam <110990848+ibkam@users.noreply.github.com>
2023-10-25 11:19:02 +0300

    Delete 0x12-javascript-warm_up directory
    Studies requirement after deferment period

--- 0x12-javascript-warm_up/10-factorial.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-// computes and prints a factorial
-const { argv } = require('process');
-const n = argv[2];
-function factorial (n) {
-  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
-}
-console.log(factorial(Number(n)));
--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-//  prints two arguments passed to it, in the following format: “ is ”
-const { argv } = require('process');
-const argv1 = argv[2];
-const argv2 = argv[3];
-console.log(argv1 + ' is ' + argv2);
