9a3fe51fd897fcade69064286682e78bd496284b
ibkam <110990848+ibkam@users.noreply.github.com>
2023-10-25 11:09:03 +0300

    Delete 0x0B-python-input_output directory
    Studies requirement after deferment

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Function that reads a textfile in UTF8"""
-
-
-def read_file(filename=""):
-    """print the content of UTF8 file to stdout"""
-    with open(filename, encoding="utf-8") as myFile:
-        read_data = myFile.read()
-        print(read_data, end="")
