125238198529fe75ae046fb461d1aa71e1ff8f7c
ibkam <110990848+ibkam@users.noreply.github.com>
2023-10-25 11:23:13 +0300

    Delete 0x14-javascript-web_scraping directory
    Studies requirement after deferment period

--- 0x14-javascript-web_scraping/1-writeme.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-// script that writes a string to a file
-const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error) {
-    console.log(error);
-  }
-});
