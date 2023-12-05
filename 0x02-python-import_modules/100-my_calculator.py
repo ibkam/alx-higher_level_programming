24301eeb9f462579ce721424e239433a9e953b42
ibkam <ibrahimkamau254@gmail.com>
2023-12-05 04:59:42 -0500

    Update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,7 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div
     import sys
 
