514a4360c65fb7c7b4d00abd03297a5b27868574
ibkam <ibrahimkamau254@gmail.com>
2023-12-05 03:57:19 -0500

    Prints name in a compiled module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+
+    # iterate through dir module to print names
+    for name in dir(hidden_4):
+        if name[:2] != "__":
+            print(name)
