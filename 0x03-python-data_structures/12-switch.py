8341a25e94ba7f156747b4e285ed2144870b8c55
ibkam <ibrahimkamau254@gmail.com>
2023-12-06 08:48:44 -0500

    Switch

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list) - 1:
-        return (my_list)
-    else:
-        my_list.remove(my_list[idx])
-    return (my_list)
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
+
