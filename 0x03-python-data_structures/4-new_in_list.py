7f83c4c597aa949ea01c0b82d9001a7e4975fc75
ibkam <ibrahimkamau254@gmail.com>
2023-12-06 05:21:32 -0500

    Replace an element without modifying orginal list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,10 +5,8 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
 
-    for i in range(len(my_list) - 1):
-        if i != idx:
-            # append new_list
-            new_list.append(my_list[i])
-    else:
-        new_list.append(element)
-        return new_list
+    new_list = my_list.copy()
+
+    new_list[idx] = element
+
+    return new_list
