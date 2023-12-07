098b3660a9d630c1255200c31280f1f3b40b99c5
ibkam <ibrahimkamau254@gmail.com>
2023-12-07 08:01:30 -0500

    Print dtring in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for a in str:
+        if ord(a) in range(97, 123):
+            a = chr(ord(a) - 32)
+        print("{}".format(a), end="")
+    else:
+        print("")
