abef91856d990893044915775c7a00503e561058
ibkam <ibkam@DESKTOP-QSBFCOQ>
2024-01-20 14:31:33 +0300

    Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,8 +6,6 @@ class LockedClass():
     """Prevents attribute creation unless attribute = firs_name"""
     __slots__ = ("first_name")
 
-
-
     def __init__(self):
         """init method"""
         pass
