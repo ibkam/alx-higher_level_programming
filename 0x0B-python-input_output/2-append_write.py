d165d6a15b295d8fedd32d5dd0a048771fe64873
ibkam <ibrahimkamau254@gmail.com>
2024-01-22 03:30:04 -0500

    Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Function that appends a string at the end of 
-a text file (UTF8) and 
+Function that appends a string at the end of
+a text file (UTF8) and
 returns the number of characters added
 """
 
