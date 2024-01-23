688c6d659e52fc5bdf74f5988f90c474450bf65d
ibkam <ibrahimkamau254@gmail.com>
2024-01-23 07:41:07 -0500

    First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -19,9 +19,24 @@ class Rectangle(Base):
             height (int): The height of the new Rectangle
             x (int): The x coordinate of the new Rectangle
             y (int): The identity of the new Rectangle
+        Raises:
+            TypeError: if either of width or height is not an int
+            TypeError: if either of width or height <= 0
+            TypeErrror: if either of x or y is not an int
+            ValueError: if either of x or y < 0
         """
         self.width = width
         self.height = height
         self.x = x
         self.y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """set width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError(
