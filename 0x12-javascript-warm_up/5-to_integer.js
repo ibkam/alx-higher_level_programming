c3ab74525041d7711d3214ba195ec7c44b60557e
ibkam <110990848+ibkam@users.noreply.github.com>
2024-03-11 14:59:44 +0300

    An Integer
    script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
    
    If the argument can’t be converted to an integer, print “Not a number”
    You must use console.log(...) to print all output
    You are not allowed to use var
    You are not allowed to use try/catch

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// prints My number: <first argument converted in integer>const { argv } = require('process');
+const { argv } = require('process');
+const convert = Number(argv[2]);
+
+if (isNaN(convert)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${convert}`);
+}
