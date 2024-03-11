dc7e2a5952565f2fb43a6da93fc69dcc30d2a87b
ibkam <110990848+ibkam@users.noreply.github.com>
2024-03-11 14:54:32 +0300

    Arguments
    script that prints a message depending of the number of arguments passed:
    
    If no arguments are passed to the script, print “No argument”
    If only one argument is passed to the script, print “Argument found”
    Otherwise, print “Arguments found”
    You must use console.log(...) to print all output
    You are not allowed to use var

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+//  prints a message depending of the number of arguments passed:
+const process = require('process');
+const args = process.argv;
+if (args.length === 2) {
+  console.log('No argument');
+} else if (args.length < 4) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
