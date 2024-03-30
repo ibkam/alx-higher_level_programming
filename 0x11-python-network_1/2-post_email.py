f3324b0ba1e025cbd8eda5d6657543415f0f7e83
ibkam <ibrahimkamau254@gmail.com>
2024-03-30 12:14:10 +0300

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -16,6 +16,5 @@ if __name__ == "__main__":
     value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
 
-    #request = urllib.request.Request(url, data)
     with urllib.request.urlopen(url, data) as response:
         print(response.read().decode("utf-8"))
