6a323dbcb118c8b0c6a6b639ede9bcdfadd3a1ec
ibkam <ibrahimkamau254@gmail.com>
2024-03-30 11:57:13 +0300

    Fetch & header value

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# python script that takes in a URL, sends a request to the URL,
+# and displays the value of the 'X-Request-Id',
+# variable found in the header of the response.
+"""
+   send request to a URL, & displays value of the 'X-Request-Id'
+"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(dict(response.headers).get("X-Request-Id"))
