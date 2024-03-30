e10aeed050998464961b4b4d96095da47786b2e7
ibkam <ibrahimkamau254@gmail.com>
2024-03-30 11:38:10 +0300

    Fetch Url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# python script that fetches 'https://alu-intranet.hbtn.io/status'
+"""
+    fetch 'https://alx-intranet.hbtn.io/status'
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
