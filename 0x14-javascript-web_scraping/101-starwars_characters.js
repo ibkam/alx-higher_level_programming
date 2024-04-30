33b21d8963f352e6f16808988b2171cd74763719
ibkam <ibrahimkamau254@gmail.com>
2024-04-30 15:32:50 +0300

    prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,33 +4,22 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, async (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
     const content = JSON.parse(body);
     const characters = content.characters;
-    
+    // console.log(characters);
     for (const character of characters) {
-      try {
-        const characterResponse = await getRequest(character);
-        console.log(JSON.parse(characterResponse).name);
-      } catch (error) {
-        console.log(error);
-      }
+      request.get(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const names = JSON.parse(body);
+          console.log(names.name);
+        }
+      });
     }
   }
 });
-
-// Function to make request and return a promise
-function getRequest(url) {
-  return new Promise((resolve, reject) => {
-    request.get(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(body);
-      }
-    });
-  });
-}}
