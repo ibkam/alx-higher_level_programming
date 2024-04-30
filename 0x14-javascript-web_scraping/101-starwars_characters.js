537998c8e9fd2c67429f1ffdf70925b96ad8cdaa
ibkam <ibrahimkamau254@gmail.com>
2024-04-30 15:27:52 +0300

    prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,34 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
-    }
-  }
-});
+// Function to fetch characters of a Star Wars movie by ID
+function getCharacters (movieId) {
+  return new Promise((resolve, reject) => {
+    const url = `https://swapi.dev/api/films/${movieId}/`;
+
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const film = JSON.parse(body);
+        const characters = film.characters;
+        resolve(characters);
+      }
+    });
+  });
+}
+
+// Function to get character name from URL
+function getCharacterName (characterUrl) {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const character = JSON.parse(body);
+        resolve(character.name);
+      }
+    });
+  });
+}
