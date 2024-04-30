d48f79e90703ed6dc3f333556b55712053c35b9e
ibkam <ibrahimkamau254@gmail.com>
2024-04-30 11:31:04 +0300

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 // Read a file
 
-
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	console.log(data);
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
 });
